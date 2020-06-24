4c9b1794de187d2dcb621b9b57e0a9dbc9eac144
Dale <mightymuddy7@gmail.com>
2020-06-24 12:32:00 +1000

    Added jQuery for http requests, server has CORS blocking me on chrome tho

--- vueComponent/app.js
+++ vueComponent/app.js
@@ -15,9 +15,12 @@ var app6 = new Vue({
   },
   methods: {
     search: function () {
-      console.log(this.name);
-      console.log(this.gender);
-      console.log(this.city);
+		console.log(this.name);
+		console.log(this.gender);
+		console.log(this.city);
+		$.get( "http://127.0.0.1:8000/getContacts/", function( data ) {
+			console.log(data);
+		});
     }
   }
 })
