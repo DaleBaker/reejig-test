e34178a5961d09a693b0605a17611f1c1cea0d58
Dale <mightymuddy7@gmail.com>
2020-06-24 12:29:22 +1000

    Added html components for the search

--- vueComponent/app.js
+++ vueComponent/app.js
@@ -1,6 +1,24 @@
-new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello yeet Vue.js!'
+
+var header = new Vue({
+  el: '#header',
+  data: {
+    message: 'Some header.'
+  }
+});
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    name: 'a!',
+    gender: 'b!',
+    city: 'c!',
+  },
+  methods: {
+    search: function () {
+      console.log(this.name);
+      console.log(this.gender);
+      console.log(this.city);
     }
+  }
 })
+
